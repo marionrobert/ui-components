46e2760644e1a62b85c006bc9bc8fcdac3bcb4d7
Marion Robert <mrobert148@gmail.com>
2024-04-29 15:59:02 -0400

    detect when grid is in the view with react-intersection-observer

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
+"use client";
 import Image from "next/image";
 import logo from "../public/next.svg";
+import { useEffect } from "react";
+import { useInView } from 'react-intersection-observer';
 
 
 const colors: string[] = [
@@ -13,7 +16,30 @@ const colors: string[] = [
   '#00008B', // Bleu foncé
 ];
 
+const gridReferences: { [key: string]: string[] } = {
+  "1": ["1", "1"],
+  "2": ["1", "2"],
+  "3": ["1", "3"],
+  "4": ["2", "1"],
+  "5": ["2", "2"],
+  "6": ["2", "3"],
+  "7": ["3", "1"],
+  "8": ["3", "2"],
+  "9": ["3", "3"],
+};
+
+
 export default function Home() {
+  const { ref, inView } = useInView();
+
+  const handleGridContainerInView = () => {
+    console.log('Le grid-container est dans la vue.');
+  };
+
+  const handleGridContainerNotInView = () => {
+    console.log('Le grid-container n\'est plus dans la vue.');
+  };
+
   const positions = [];
   for (let row = 1; row <= 3; row++) {
     for (let col = 1; col <= 3; col++) {
@@ -24,11 +50,26 @@ export default function Home() {
     }
   }
 
+  useEffect(() => {
+    if (inView) {
+      handleGridContainerInView();
+    } else {
+      handleGridContainerNotInView();
+    }
+  }, [inView]);
+
   return (
     <main>
-      <div className="grid-container">
+      <p>lbjld</p>
+      <p>lbjld</p>
+      <p>lbjld</p>
+      <p>lbjld</p>
+      <p>lbjld</p>
+      <p>lbjld</p>
+
+      <div ref={ref} className="grid-container">
         {positions.map((pos, index) => (
-          <div key={index} className={`cell grid-item-${index +1}`} style={{ gridRowStart: pos.row, gridColumnStart: pos.col, backgroundColor: colors[index] }}>
+          <div key={index} className={`cell grid-item-${index+1}`} style={{ gridRowStart: pos.row, gridColumnStart: pos.col, backgroundColor: colors[index] }}>
             {pos.index + 1}
           </div>
         ))}
