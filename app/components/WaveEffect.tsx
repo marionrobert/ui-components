9756ee058694d3530d01ff39662e11e9f7af4a0d
Marion Robert <mrobert148@gmail.com>
2024-05-22 11:10:14 -0400

    waveEffect

---
+++ app/components/WaveEffect.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function WaveEffect(){
+  return (
+    <div className="container">
+      <h2>Wave animation</h2>
+      <div className="water_wave">
+      </div>
+    </div>
+  );
+}
